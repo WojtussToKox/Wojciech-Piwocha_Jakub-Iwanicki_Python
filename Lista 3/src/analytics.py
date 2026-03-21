77497f787c2849f845ceadf5a0409a91e186c79f
Wojciech <wojtek.p8@op.pl>
2026-03-21 22:59:44 +0100

    Dodano reszte zadan analitycznych

--- Lista 3/src/analytics.py
+++ Lista 3/src/analytics.py
@@ -1,7 +1,11 @@
 from collections import Counter
 from collections import defaultdict
+from urllib.parse import urlparse
+import os
+
 
 LOG_KEYS = ["ts", "uid", "host_orig", "port_orig", "host_resp", "port_resp", "method", "host", "uri", "status_code"]
+
 
 # Zadanie 7
 def getTopIps(log, n=10):
@@ -14,6 +18,7 @@ def getTopIps(log, n=10):
     # 3. Metoda most_common(n) zwraca posortowaną listę krotek [(ip1, ilosc1), (ip2, ilosc2), ...]
     return counted_ip.most_common(n)
 
+
 # Zadanie 11
 def getTopUris(log, n=10):
     # 1. Wyciągamy same adresy URI (indeks 8) ze wszystkich krotek w logu
@@ -22,9 +27,9 @@ def getTopUris(log, n=10):
     # 2. Counter automatycznie zlicza wystąpienia każdego IP
     counted_uri = Counter(all_uri)
 
-
     # 3. Z listy krotek [(uri1, ilosc1), (uri2, ilosc2), ...] wyciągamy tylko Uri
     return [uri for uri, count in counted_uri.most_common(n)]
+
 
 # Zadanie 13
 def entryToDict(entry):
@@ -32,6 +37,7 @@ def entryToDict(entry):
     # Np. ('ts', entry[0]), ('uid', entry[1]) itd. Zatrzymuje się na najkrótszej liście.
     # dict() przyjmuje te pary i konwertuje je na gotowy słownik {klucz: wartość}.
     return dict(zip(LOG_KEYS, entry))
+
 
 # Zadanie 14
 def logToDict(log):
@@ -46,6 +52,7 @@ def logToDict(log):
     # Ewentualne rzutowanie na zwyły słownik
     return sessions
 
+
 # Zadanie 15
 def printDictEntryDates(log_dict):
     # Przechodzimy przez słownik
@@ -54,8 +61,10 @@ def printDictEntryDates(log_dict):
         request_count = len(entries)
 
         # Adresy IP (wyciągamy unikalne i pozbywamy się ewentualnych braków '-')
-        host_orig_set = {entry['host_orig'] for entry in entries if entry.get('host_orig') and entry['host_orig'] != "-"}
-        host_resp_set = {entry['host_resp'] for entry in entries if entry.get('host_resp') and entry['host_resp'] != "-"}
+        host_orig_set = {entry['host_orig'] for entry in entries if
+                         entry.get('host_orig') and entry['host_orig'] != "-"}
+        host_resp_set = {entry['host_resp'] for entry in entries if
+                         entry.get('host_resp') and entry['host_resp'] != "-"}
 
         # Daty pierwszego i ostatniego żadania
 
@@ -67,11 +76,10 @@ def printDictEntryDates(log_dict):
         else:
             first_req, last_req = "Brak danych", "Brak danych"
 
-
         methods_counter = Counter([entry['method'] for entry in entries])
 
         codes_2xx_count = sum(1 for entry in entries if 200 <= entry.get("method") < 300)
-        codes_2xx_percent = (codes_2xx_count/request_count) * 100 if request_count > 0 else 0
+        codes_2xx_percent = (codes_2xx_count / request_count) * 100 if request_count > 0 else 0
 
         # --- FORMATOWANIE WYJŚCIA ---
         print(f"=== Sesja UID: {uid} ===")
@@ -88,6 +96,7 @@ def printDictEntryDates(log_dict):
         print(f"Kody 2xx: {codes_2xx_count}/{request_count} ({codes_2xx_percent:.2f}%)")
         print("-" * 50)
 
+
 # Zadanie 16a
 def getMostActiveSessionFromDict(log_dict):
     if not log_dict:
@@ -97,7 +106,96 @@ def getMostActiveSessionFromDict(log_dict):
 
     return winner_uid, len(winner_logs)
 
+
 # Zadanie 16b
 def getMostActiveSessionFromLog(log):
     counts = Counter(entry[1] for entry in log)
     return counts.most_common(1)[0]
+
+
+# Zadanie 17
+def getSessionPaths(log):
+    # Słownik, który automatycznie inicjuje pustą listę dla nowego uid
+    sessions_paths = defaultdict(list)
+
+    for entry in log:
+        uid = entry[1]
+        path = entry[8]
+
+        sessions_paths[uid].append(path)
+
+    # Ewentualne rzutowanie na zwyły słownik
+    # return dict(sessions_paths)
+    return sessions_paths
+
+
+# Zadanie 18
+def detectSus(log, threshold, min_404_ratio=0.0, max_interval_sec=None):
+    # Słownik do zbierania statystyk dla każdego IP
+    ip_data = defaultdict(lambda: {"count": 0, "404_count": 0, "timestamps": []})
+
+    for entry in log:
+        ts = entry[0]  # ts (Obiekt datetime)
+        ip = entry[2]  # id.orig_h (Adres IP klienta)
+        status = entry[9]  # status_code
+
+        ip_data[ip]["count"] += 1
+        if status == 404:
+            ip_data[ip]["404_count"] += 1
+        ip_data[ip]["timestamps"].append(ts)
+
+    sus_ip = {}
+
+    # 2. Analiza zebranych danych
+    for ip, data in ip_data.items():
+        count = data["count"]
+
+        # czy przekroczył próg zapytań?
+        if count >= threshold:
+
+            # Obliczanie odsetka błędów 404
+            ratio_404 = data["404_count"] / count
+
+            ts_sorted = sorted(data["timestamps"])
+
+            # Różnica czasu między pierwszym a ostatnim zapytaniem w sekundach
+            time_span = (ts_sorted[-1] - ts_sorted[0]).total_seconds()
+
+            avg_interval = time_span / count if count >= 1 else 0
+
+            is_404_sus = (ratio_404 >= min_404_ratio)
+            is_time_sus = (avg_interval <= max_interval_sec) if max_interval_sec is not None else True
+
+            # Jeśli IP spełnia wszystkie nałożone filtry, trafia na listę podejrzanych
+            if is_404_sus and is_time_sus:
+                sus_ip[ip] = {
+                    "liczba_zadan": count,
+                    "bledy_404_proc": round(ratio_404 * 100, 2),  # Zapisane w procentach
+                    "sredni_odstep_sek": round(avg_interval, 3)
+                }
+
+    return sus_ip
+
+
+# Zadanie 19
+def get_extension_stats(log):
+    extensions = []
+
+    for entry in log:
+        uri = entry[8]  # Indeks 8 to URI
+
+        # Pozbywamy się parametrów zapytania (wszystkiego po '?' i '#')
+        # Z '/sciezka/plik.html?user=123' zostanie nam '/sciezka/plik.html'
+        clean_path = urlparse(uri).path
+
+        # splitext dzieli na: ('/sciezka/plik', '.html')
+        _, extension = os.path.splitext(clean_path)
+
+        if extension:
+            # ".JPG" -> "jpg"
+            clean_ext = extension[1:].lower()
+
+            extensions.append(clean_ext)
+
+    # Counter policzy nam wszystkie elementy, a dict() zamieni wynik na zwykły słownik
+    return dict(Counter(extensions))
